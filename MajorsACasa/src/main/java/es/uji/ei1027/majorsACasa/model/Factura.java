98132c8ac8f7e6c119990ea95c87f64cadf78fa3
al375884 <al375884@uji.es>
2020-03-06 15:59:28 +0100

    clases de model creadas

---
+++ MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/model/Factura.java
@@ -0,0 +1,53 @@
+package es.uji.ei1027.majorsACasa.model;
+
+public class Factura {
+	private String idFactura;
+	private String idBeneficiario;
+	//private Date fechaCreacion;
+	//private Date fechaInicio;
+	//private Date fechaFin;
+	private float cantidad;
+	private float concepto;
+	
+	public Factura(){
+	}
+	
+	public String getIdFactura() {
+		return idFactura;
+	}
+	
+	public void setIdFactura(String idFactura) {
+		this.idFactura = idFactura;
+	}
+	
+	public String getIdBeneficiario() {
+		return idBeneficiario;
+	}
+	
+	public void setIdBeneficiario(String idBeneficiario) {
+		this.idBeneficiario = idBeneficiario;
+	}
+	
+	public float getCantidad() {
+		return cantidad;
+	}
+	
+	public void setCantidad(float cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public float getConcepto() {
+		return concepto;
+	}
+	
+	public void setConcepto(float concepto) {
+		this.concepto = concepto;
+	}
+
+	@Override
+	public String toString() {
+		return "Factura [idFactura=" + idFactura + ", idBeneficiario=" + idBeneficiario + ", cantidad=" + cantidad
+				+ ", concepto=" + concepto + "]";
+	}
+	
+}
