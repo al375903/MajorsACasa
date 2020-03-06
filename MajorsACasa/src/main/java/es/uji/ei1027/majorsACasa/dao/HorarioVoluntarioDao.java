331cd27dd6220c4fa26907bf09a1a60892c5378e
al375842 <al375842@uji.es>
2020-03-06 16:33:44 +0100

    Dao y RowMapper de HorarioVoluntario (sin los atributos conflictivos Date y Time)

--- MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/dao/HorarioVoluntarioDao.java
+++ MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/dao/HorarioVoluntarioDao.java
@@ -1,5 +1,68 @@
 package es.uji.ei1027.majorsACasa.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import es.uji.ei1027.majorsACasa.model.HorarioVoluntario;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class HorarioVoluntarioDao {
 
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public void setDatSource(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	void addHorarioVoluntario(HorarioVoluntario horarioVoluntario) {
+		jdbcTemplate.update("INSERT INTO HorarioVoluntario VALUES(?,?,?,?,?,?,?)",
+			horarioVoluntario.getIdHorario(), horarioVoluntario.getIdVoluntario(),
+			horarioVoluntario.getIdBeneficiario(), null, null,
+			null, horarioVoluntario.isLibre());
+	}
+	
+	void deleteHorarioVoluntario(HorarioVoluntario horarioVoluntario) {
+		jdbcTemplate.update("DELETE FROM HorarioVoluntario WHERE idVoluntario=?", horarioVoluntario.getIdHorario());
+	}
+	
+	void updateHorarioVoluntario(HorarioVoluntario horarioVoluntario) {
+		jdbcTemplate.update("UPDATE HorarioVoluntario SET fecha=?, horaInicio=?, horaFin=?, libre=? WHERE idHorario=?",
+				null, null, null, horarioVoluntario.isLibre());
+	}
+	
+	HorarioVoluntario getHorarioVoluntario(String idHorario) {
+		try {
+			return jdbcTemplate.queryForObject("SELECT * FROM HorarioVoluntario WHERE idHorario=?",
+					new HorarioVoluntarioRowMapper(), idHorario);
+		} catch(EmptyResultDataAccessException e) {
+			return null;
+		}
+	}
+	
+	List<HorarioVoluntario> getHorariosVoluntarios(){
+		try {
+			return jdbcTemplate.query("SELECT * FROM HorarioVoluntario", new HorarioVoluntarioRowMapper());
+		} catch(EmptyResultDataAccessException e) {
+			return new ArrayList<HorarioVoluntario>();
+		}
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
--- MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/dao/HorarioVoluntarioRowMapper.java
+++ MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/dao/HorarioVoluntarioRowMapper.java
@@ -1,5 +1,20 @@
 package es.uji.ei1027.majorsACasa.dao;
 
-public class HorarioVoluntarioRowMapper {
+import org.springframework.jdbc.core.RowMapper;
 
+import es.uji.ei1027.majorsACasa.model.HorarioVoluntario;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public final class HorarioVoluntarioRowMapper implements RowMapper<HorarioVoluntario>{
+	public HorarioVoluntario mapRow(ResultSet rs, int rowNum) throws SQLException{
+		HorarioVoluntario horarioVoluntario = new HorarioVoluntario();
+		horarioVoluntario.setIdHorario(rs.getString("idHorario"));
+		horarioVoluntario.setIdVoluntario(rs.getString("idVoluntario"));
+		horarioVoluntario.setIdBeneficiario(rs.getString("idBeneficiario"));
+		// Aquí debería haber 3 sentencias para los atributos de tipo Date y Time
+		horarioVoluntario.setLibre(rs.getBoolean("libre"));
+		return horarioVoluntario;
+	}
 }
