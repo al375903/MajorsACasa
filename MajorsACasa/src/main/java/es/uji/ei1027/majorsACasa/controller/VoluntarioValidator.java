83ddb6cc2bdf74a60d738db62353679138e6bf63
Lucas <Lucas@DESKTOP-IRQDKFV>
2020-04-22 18:17:14 +0200

    Correcciones VoluntarioValidator.

--- MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/controller/VoluntarioValidator.java
+++ MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/controller/VoluntarioValidator.java
@@ -23,35 +23,35 @@ public class VoluntarioValidator implements Validator {
 	public void validate(Object obj, Errors errors) {
 		 Voluntario voluntario = (Voluntario)obj;
 		 if (voluntario.getIdVoluntario().trim().equals(""))
-			 errors.rejectValue("idVoluntario", "obligatorio", "Introduzca un Id");
+			 errors.rejectValue("idVoluntario", "obligatorio", "Introduzca un Id.");
 		 
 		 if (voluntario.getIdVoluntario().trim().length() < 9)
-			 errors.rejectValue("idVoluntario", "minimo", "Debe tener al menos 9 caracteres");
+			 errors.rejectValue("idVoluntario", "minimo", "Debe tener al menos 9 caracteres.");
 		 
 		 if (voluntario.getNombre().trim().equals(""))
-			 errors.rejectValue("nombre", "obligatorio", "Introduzca un nombre");
+			 errors.rejectValue("nombre", "obligatorio", "Introduzca un nombre.");
 		 
 		 if (voluntario.getEmail().trim().equals(""))
-			 errors.rejectValue("email", "obligatorio", "Introduzca un e-mail");
+			 errors.rejectValue("email", "obligatorio", "Introduzca un e-mail.");
 		 
 		 if (voluntario.getDireccion().trim().equals(""))
-			 errors.rejectValue("direccion", "obligatorio", "Introduzca una direccion");
+			 errors.rejectValue("direccion", "obligatorio", "Introduzca una direccion.");
 		 
 		 if (voluntario.getFechaPeticionVoluntariado().equals(null))
-			 errors.rejectValue("fechaPeticionVoluntariado", "obligatorio", "Introduzca la fecha de peticion de voluntariado");
+			 errors.rejectValue("fechaPeticionVoluntariado", "obligatorio", "Introduzca la fecha de peticion de voluntariado.");
 		 
 		 if (voluntario.getFechaFin().equals(null))
-			 errors.rejectValue("fechaFin", "obligatorio", "Introduzca la fecha fin");
+			 errors.rejectValue("fechaFin", "obligatorio", "Introduzca la fecha fin.");
 		 
 		 if (voluntario.getDireccion().trim().equals(""))
-			 errors.rejectValue("direccion", "obligatorio", "Introduzca una direccion");
+			 errors.rejectValue("direccion", "obligatorio", "Introduzca una direccion.");
 		 
 		 List<String> valores = Arrays.asList("Femenino", "Masculino");
 		 if(!valores.contains(voluntario.getGenero()))
-			 errors.rejectValue("genero", "valor incorrecto", "Seleccione el tipo de género");
+			 errors.rejectValue("genero", "valor incorrecto", "Seleccione el género.");
 		 
 		 if(voluntario.getEdad() < 18)
-			 errors.rejectValue("edad",  "fuera de rango", "Debe ser igual o mayor a 18");
+			 errors.rejectValue("edad",  "fuera de rango", "Debe ser igual o mayor a 18.");
 		 
 		 
 	}
