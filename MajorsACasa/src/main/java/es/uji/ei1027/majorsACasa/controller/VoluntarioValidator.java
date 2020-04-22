5dbdeb58f78298501b1bb6df3e7684bf3e00e9ab
Usuario <Usuario@DESKTOP-8PH9L56>
2020-04-22 18:14:27 +0200

    Validador voluntario

--- MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/controller/VoluntarioValidator.java
+++ MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/controller/VoluntarioValidator.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+
 import es.uji.ei1027.majorsACasa.model.Voluntario;
 
 
@@ -20,6 +21,38 @@ public class VoluntarioValidator implements Validator {
 	
 	@Override
 	public void validate(Object obj, Errors errors) {
-		Voluntario factura = (Voluntario)obj;
+		 Voluntario voluntario = (Voluntario)obj;
+		 if (voluntario.getIdVoluntario().trim().equals(""))
+			 errors.rejectValue("idVoluntario", "obligatorio", "Introduzca un Id");
+		 
+		 if (voluntario.getIdVoluntario().trim().length() < 9)
+			 errors.rejectValue("idVoluntario", "minimo", "Debe tener al menos 9 caracteres");
+		 
+		 if (voluntario.getNombre().trim().equals(""))
+			 errors.rejectValue("nombre", "obligatorio", "Introduzca un nombre");
+		 
+		 if (voluntario.getEmail().trim().equals(""))
+			 errors.rejectValue("email", "obligatorio", "Introduzca un e-mail");
+		 
+		 if (voluntario.getDireccion().trim().equals(""))
+			 errors.rejectValue("direccion", "obligatorio", "Introduzca una direccion");
+		 
+		 if (voluntario.getFechaPeticionVoluntariado().equals(null))
+			 errors.rejectValue("fechaPeticionVoluntariado", "obligatorio", "Introduzca la fecha de peticion de voluntariado");
+		 
+		 if (voluntario.getFechaFin().equals(null))
+			 errors.rejectValue("fechaFin", "obligatorio", "Introduzca la fecha fin");
+		 
+		 if (voluntario.getDireccion().trim().equals(""))
+			 errors.rejectValue("direccion", "obligatorio", "Introduzca una direccion");
+		 
+		 List<String> valores = Arrays.asList("Femenino", "Masculino");
+		 if(!valores.contains(voluntario.getGenero()))
+			 errors.rejectValue("genero", "valor incorrecto", "Seleccione el tipo de género");
+		 
+		 if(voluntario.getEdad() < 18)
+			 errors.rejectValue("edad",  "fuera de rango", "Debe ser igual o mayor a 18");
+		 
+		 
 	}
 }
