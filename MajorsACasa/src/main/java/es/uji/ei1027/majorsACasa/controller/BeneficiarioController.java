f361815a5c6f3e9b3b06da6a5530e2cee01220e4
Usuario <Usuario@DESKTOP-8PH9L56>
2020-05-23 18:05:11 +0200

    Indice de beneficiario

--- MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/controller/BeneficiarioController.java
+++ MajorsACasa/src/main/java/es/uji/ei1027/majorsACasa/controller/BeneficiarioController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import es.uji.ei1027.majorsACasa.dao.BeneficiarioDao;
+import es.uji.ei1027.majorsACasa.dao.PeticionDao;
 import es.uji.ei1027.majorsACasa.model.Beneficiario;
+import es.uji.ei1027.majorsACasa.model.Peticion;
 import es.uji.ei1027.majorsACasa.model.UserDetails;
 import es.uji.ei1027.majorsACasa.services.PeticionService;
 
@@ -27,6 +29,13 @@ public class BeneficiarioController {
 	@Autowired
 	public void setBeneficiarioDao(BeneficiarioDao beneficiarioDao) {
 		this.beneficiarioDao = beneficiarioDao;
+	}
+	
+	private PeticionDao peticionDao;
+	
+	@Autowired
+	public void setPeticionDao(PeticionDao peticionDao) {
+		this.peticionDao = peticionDao;
 	}
 	
 	private PeticionService peticionService;
@@ -79,6 +88,39 @@ public class BeneficiarioController {
 		return "redirect:list";
 	}
 	
+	@RequestMapping(value="addPeticion")
+	public String addPeticion(Model model) {
+		model.addAttribute("peticion", new Peticion());
+		return "beneficiario/addPeticion";
+	}
+	
+	@RequestMapping(value="/addPeticion", method=RequestMethod.POST)
+	public String processAddSubmit(@ModelAttribute("peticion") Peticion peticion,
+									BindingResult bindingResult) {
+		PeticionValidator peticionValidator = new PeticionValidator();
+		peticionValidator.validate(peticion, bindingResult);
+		if(bindingResult.hasErrors())
+			return "beneficiario/addPeticion";
+		try {
+			for(Peticion peticion2 : peticionDao.getPeticiones()) {
+				if(peticion2.getIdBeneficiario().equals(peticion.getIdBeneficiario())) {
+					if(peticion2.getEstado().equals("Aceptada") || peticion2.getEstado().equals("NoRevisada")) {
+						throw new MajorsACasaException(
+								"No puede realizar una nueva petición teniendo una petición aceptada o por revisar", "ErrorCrearPetición");
+					}
+				}
+			}
+			peticionDao.addPeticion(peticion);
+		} catch (DuplicateKeyException e) {
+			throw new MajorsACasaException(
+					"Ya existe una petición con id " + peticion.getIdPeticion(), "CPduplicada");
+		} catch (DataAccessException e) {
+			throw new MajorsACasaException(
+					"Error en el acceso a la base de datos", "ErrorAccediendoDatos");
+		}
+		return "redirect:index";
+	}
+	
 	@RequestMapping(value="/update/{id}", method = RequestMethod.GET) 
 	public String editBeneficiario(HttpSession session, Model model, @PathVariable String id) { 
 		UserDetails user = (UserDetails)session.getAttribute("user");
@@ -114,8 +156,16 @@ public class BeneficiarioController {
 		return "redirect:../list";
 	}
 	
-
-	
+	@RequestMapping("/index")
+	public String indexBeneficiario(HttpSession session, Model model) {
+		UserDetails user = (UserDetails)session.getAttribute("user");
+	    if (user == null || !(user.getTipo().equals("jefe") || user.getTipo().equals("beneficiario"))) { 
+          model.addAttribute("user", new UserDetails());
+          session.setAttribute("nextUrl", "beneficiario/index");
+          return "login";
+        }
+		return "beneficiario/index";
+	}
 	
 	@RequestMapping("/peticiones")
 	public String listPeticionesBeneficiario(HttpSession session, Model model) {
